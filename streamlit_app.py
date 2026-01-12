8932328a47e94a99491b62f40f88a64129c35899
Nairisz <norizzaty.norsupardi@gmail.com>
2026-01-13 00:15:18 +0800

    Remove duplicate set_page_config call

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from pso_core import run_pso, repair_solution
 
+st.set_page_config(layout="wide")
 with open("theme.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-st.set_page_config(layout="wide")
 st.title("📘 PSO Exam Timetabling Dashboard")
 
 st.markdown("---")
